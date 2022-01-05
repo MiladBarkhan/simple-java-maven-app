723c97ce299e744acc3792fbff0341fc63473c37
Mahdi Sharifi <picher_s@yahoo.com>
2022-01-05 12:46:53 +0330

    Add Context

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,8 +21,8 @@ public class App {
     static final Logger logger= LoggerFactory.getLogger(App.class);
     public static void main(String[] args) {
         logger.info("#context is starting....");
-       SpringApplication.run(App.class,args);
-        logger.info("#context is started....");
+        ApplicationContext context = SpringApplication.run(App.class, args);
+        logger.info("#context is started...." + context.getEnvironment());
     }
 
     @PostConstruct
